87ad333ddceee1d48c321ca88b50f9dc23c41b7c
Ved Thakker <thakkerved5@gmail.com>
2025-03-05 02:55:44 +0530

    socket

---
+++ chatting/server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Update with frontend URL
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/chatApp")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+// Message Schema
+const messageSchema = new mongoose.Schema({
+  room: String,
+  sender: String,
+  text: String,
+  timestamp: { type: Date, default: Date.now },
+});
+const Message = mongoose.model("Message", messageSchema);
+
+// WebSocket logic
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("joinRoom", async ({ room }) => {
+    console.log(`User joined room: ${room}`);
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+
+    // Send past messages
+    const messages = await Message.find({ room });
+    socket.emit("loadMessages", messages);
+  });
+
+  socket.on("sendMessage", async ({ room, sender, text }) => {
+    console.log(`Message received from ${sender}: ${text}`);
+    const message = new Message({ room, sender, text });
+    await message.save();
+
+    io.to(room).emit("receiveMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Socket.io server is running.");
+});
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import SpeechRecognitionComponent from './pages/Recognition';
-import BrokerLogin from './pages/BrokerLogin';
-import BrokerDash from './pages/BrokerDash';
+import SpeechRecognitionComponent from "./pages/Recognition";
+import BrokerLogin from "./pages/BrokerLogin";
+import BrokerDash from "./pages/BrokerDash";
+import Chat from "./pages/Chat";
 
 function App() {
-
-
-
   return (
     <>
-    <BrowserRouter>
-      <RoutesWeb />
-    </BrowserRouter>
+      <BrowserRouter>
+        <RoutesWeb />
+      </BrowserRouter>
     </>
   );
 }
@@ -21,17 +19,16 @@ const RoutesWeb = () => {
   const location = useLocation(); // Get the current route
 
   return (
-      <>
+    <>
       {/* {loading && <Loader />} */}
       <Routes>
         <Route path="/" element={<SpeechRecognitionComponent />} />
         <Route path="/blogin" element={<BrokerLogin />} />
         <Route path="/bdash" element={<BrokerDash />} />
-        
-        
+        <Route path="/chat" element={<Chat />} />
       </Routes>
       {/* {!isAdminRoute && <Footer />}       Conditionally render Footer */}
-      </>
+    </>
   );
 };
 
