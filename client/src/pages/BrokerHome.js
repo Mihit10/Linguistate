2d9676eb48837e96ff8bee168290a6f774b3a24f
Ved Thakker <thakkerved5@gmail.com>
2025-03-05 12:36:10 +0530

    room

--- client/src/pages/BrokerHome.js
+++ client/src/pages/BrokerHome.js
@@ -11,7 +11,7 @@ const BrokerHome = () => {
     name: "Mihit Singasane",
     language: "English",
     specialization: "Real Estate Broker",
-    uniqueCode: "112211",
+    uniqueCode: "182465",
     email: "mihit.singasane@realestate.com",
     phone: "+91 98765 43210",
   };
@@ -142,7 +142,7 @@ const BrokerHome = () => {
           </motion.div>
         </div>
       ) : (
-        <SpeechRecognitionComponent room="112211" username="broker" />
+        <SpeechRecognitionComponent room="182465" username="broker" />
       )}
     </div>
   );
