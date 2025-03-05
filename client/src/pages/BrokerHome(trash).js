9996e2d795c0f999ef15d5672c4166b320004cad
Mihit10 <mihitsingasane@gmail.com>
2025-03-05 17:21:12 +0530

    room 3

--- client/src/pages/BrokerHome(trash).js
+++ client/src/pages/BrokerHome(trash).js
@@ -11,7 +11,7 @@ const BrokerHome = () => {
     name: "Mihit Singasane",
     language: "English",
     specialization: "Real Estate Broker",
-    uniqueCode: "142536",
+    uniqueCode: "162435",
     email: "mihit.singasane@realestate.com",
     phone: "+91 98765 43210",
   };
@@ -142,7 +142,7 @@ const BrokerHome = () => {
           </motion.div>
         </div>
       ) : (
-        <SpeechRecognitionComponent room="142536" username="broker" />
+        <SpeechRecognitionComponent room="162435" username="broker" />
       )}
     </div>
   );
